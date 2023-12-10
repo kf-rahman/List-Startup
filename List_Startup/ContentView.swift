d76144d2cb4e4ae6df24f1f8f0e5f2f234b92df0
kazirahman <kazif2122r@gmail.com>
2023-12-09 22:51:45 -0500

    Initial Commit

---
+++ List_Startup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  List_Startup
+//
+//  Created by Kazi Rahman on 2023-12-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ List_Startup/List_StartupApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_StartupApp.swift
+//  List_Startup
+//
+//  Created by Kazi Rahman on 2023-12-09.
+//
+
+import SwiftUI
+
+@main
+struct List_StartupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
